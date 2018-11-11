fb911b173a860c9621a3be4f1955e86564e7ac70
Pedro Henrique G Ramalho <pedro.ramalho@basis.com.br>
2018-11-11 17:20:18 -0200

    Alteracoes

--- src/main/java/br/com/bancogeral/BancoGeral/domain/Civil.java
+++ src/main/java/br/com/bancogeral/BancoGeral/domain/Civil.java
@@ -1,11 +1,18 @@
-package br.com.bancogeral.BancoGeral.service.dto;
+package br.com.bancogeral.BancoGeral.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Date;
+@Entity
 @Table(name = "tb_registro_geral")
 public class Civil {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
     @Column(name = "registro_geral")
--- src/main/java/br/com/bancogeral/BancoGeral/service/mapper/CivilMapperDTO.java
+++ src/main/java/br/com/bancogeral/BancoGeral/service/mapper/CivilMapperDTO.java
@@ -1,11 +1,11 @@
 package br.com.bancogeral.BancoGeral.service.mapper;
 
+import br.com.bancogeral.BancoGeral.domain.Civil;
 import br.com.bancogeral.BancoGeral.repository.mapper.EntityMapper;
-import br.com.bancogeral.BancoGeral.service.dto.Civil;
 import br.com.bancogeral.BancoGeral.service.dto.CivilDTO;
 import org.mapstruct.Mapper;
 
 
 @Mapper(componentModel = "spring", uses = {CivilMapperDTO.class})
-public interface CivilMapperDTO extends EntityMapper <CivilDTO, Civil>  {
+public interface CivilMapperDTO extends EntityMapper<CivilDTO, Civil> {
 }
