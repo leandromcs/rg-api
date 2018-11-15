382f2e620fbc3ba3e74ab41d89417988d5dc9914
Leandro <leandromcs96@gmail.com>
2018-11-15 00:16:21 -0200

    Atualização da Service

--- src/main/java/br/com/bancogeral/BancoGeral/service/Service.java
+++ src/main/java/br/com/bancogeral/BancoGeral/service/Service.java
@@ -4,8 +4,11 @@ import br.com.bancogeral.BancoGeral.banco1.repository.Banco1Repository;
 import br.com.bancogeral.BancoGeral.banco2.repository.Banco2Repository;
 import br.com.bancogeral.BancoGeral.banco3.repository.Banco3Repository;
 import br.com.bancogeral.BancoGeral.domain.Civil;
+import br.com.bancogeral.BancoGeral.service.dto.CivilDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @org.springframework.stereotype.Service
@@ -28,8 +31,34 @@ public class Service {
     }
 
     //Retorna os RGs da Pessoa
+    public List<Civil> consultarRGs(CivilDTO dto){
+        Civil civil = this.banco1Repository.consultarPorRGs(dto.getNome(), dto.getDataNascimento(), dto.getNomeMae());
+        Civil civil2 = this.banco2Repository.consultarPorRGs(dto.getNome(), dto.getDataNascimento(), dto.getNomeMae());
+        Civil civil3 = this.banco3Repository.consultarPorRGs(dto.getNome(), dto.getDataNascimento(), dto.getNomeMae());
 
+        List<Civil> civils = new ArrayList<>();
+        civils.add(civil);
+        civils.add(civil2);
+        civils.add(civil3);
+        return civils;
+    }
 
+    //Verifica se existe RG para Pessoa
+    public Boolean verificarSeExisteRG(CivilDTO dto){
+        Civil civil = this.banco1Repository.consultarPorRGs(dto.getNome(), dto.getDataNascimento(), dto.getNomeMae());
+        Civil civil2 = this.banco2Repository.consultarPorRGs(dto.getNome(), dto.getDataNascimento(), dto.getNomeMae());
+        Civil civil3 = this.banco3Repository.consultarPorRGs(dto.getNome(), dto.getDataNascimento(), dto.getNomeMae());
+
+        if(civil.equals(null) && civil2.equals(null) && civil3.equals(null)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+//    public Civil retornaRGMaisAntigo(){
+//
+//    }
 
 
 }
